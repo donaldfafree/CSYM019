bf61d261d601b781a359b28c2140e0dcc4e16668
Iworh Donald <78494762+donaldfafree@users.noreply.github.com>
2021-03-06 18:32:32 +0000

    This is my fifth commit

---
+++ Week4Ex3/script2.js
@@ -0,0 +1,11 @@
+function print5 () {
+    var num1 = console.log(1);
+    var num2 = console.log(2);
+    var num3 = console.log(3);
+    var num4 = console.log(4);
+    var num5 = console.log(5);
+}
+
+
+print5 ();
+print5 ();
