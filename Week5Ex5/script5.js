4aa8fd3f7ead14c4133e6e94cfd5454756b96be4
Iworh Donald <78494762+donaldfafree@users.noreply.github.com>
2021-03-07 03:34:16 +0000

    This is my twelfth commit

---
+++ Week5Ex5/script5.js
@@ -0,0 +1,16 @@
+function changeOpacity () {
+    var element = document.getElementById('circle');
+    var circleOpacity = parseFloat(element.style.opacity);
+    element.style.opacity = circleOpacity + 0.1;
+
+}
+
+function myLoadFunction () {
+    var element = document.getElementById('circle');
+    element.addEventListener('click', changeOpacity);
+    element.style.opacity = 0.5;  
+}
+
+
+
+document.addEventListener('DOMContentLoaded', myLoadFunction);
